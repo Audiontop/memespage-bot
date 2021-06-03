fab34a375912f9f5d2040dedf254285326020642
ibrahimpakir <ibrahimpakir2004@gmail.com>
2021-06-03 13:19:58 +0200

    ddf
    sdf

--- index.js
+++ index.js
@@ -46,6 +46,28 @@ bot.on('message', function(message){
 });
 
 bot.on('message', message => {
+    if (message.content === "?porn"){
+        const embed = new Discord.MessageEmbed()
+        got('https://pornhub.com/').then(response => {
+            let content = JSON.parse(response.body);
+            let permalink = content[0].data.children[0].data.permalink;
+            let memeUrl = `https://reddit.com${permalink}`;
+            let memeImage = content[0].data.children[0].data.url;
+            let memeTitle = content[0].data.children[0].data.title;
+            let memeUpvotes = content[0].data.children[0].data.ups;
+            let memeDownvotes = content[0].data.children[0].data.downs;
+            let memeNumComments = content[0].data.children[0].data.num_comments;
+            embed.setTitle(`${memeTitle}`)
+            embed.setURL(`${memeUrl}`)
+            embed.setImage(memeImage)
+            embed.setColor('RANDOM')
+            embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+            message.channel.send(embed);
+        })
+    }
+})
+
+bot.on('message', message => {
     if (message.content === "?meme"){
         const embed = new Discord.MessageEmbed()
         got('https://www.reddit.com/r/memes/random/.json').then(response => {
